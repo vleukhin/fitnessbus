9987c4fdb387ed1a03811d24b13351941e33f09a
vleukhin <norad-2013@mail.ru>
2017-08-16 15:23:14 +0300

    фикс блока соц. сетей

--- views/fitbus/blocks/social.blade.php
+++ views/fitbus/blocks/social.blade.php
@@ -2,24 +2,24 @@
 {{--Tel: +7-926-797-5419--}}
 {{--E-mail: vleukhin@ya.ru--}}
 <div class="fitbus-social">
-    <div class="fitbus-social__item">
-        <a href="https://www.facebook.com/GO2fitMoscow" target="_blank">
+    <a href="https://www.facebook.com/GO2fitMoscow" target="_blank">
+        <div class="fitbus-social__item">
             <i class="fa fa-facebook"></i>
-        </a>
-    </div>
-    <div class="fitbus-social__item">
-        <a href="https://vk.com/go_2fit" target="_blank">
+        </div>
+    </a>
+    <a href="https://vk.com/go_2fit" target="_blank">
+        <div class="fitbus-social__item">
             <i class="fa fa-vk"></i>
-        </a>
-    </div>
-    <div class="fitbus-social__item">
-        <a href="https://www.instagram.com/go_2fit/" target="_blank">
+        </div>
+    </a>
+    <a href="https://www.instagram.com/go_2fit/" target="_blank">
+        <div class="fitbus-social__item">
             <i class="fa fa-instagram"></i>
-        </a>
-    </div>
-    <div class="fitbus-social__item">
-        <a href="https://www.youtube.com/channel/UCEIATbgl9Z4vQNXjme0sPYA" target="_blank">
+        </div>
+    </a>
+    <a href="https://www.youtube.com/channel/UCEIATbgl9Z4vQNXjme0sPYA" target="_blank">
+        <div class="fitbus-social__item">
             <i class="fa fa-youtube"></i>
-        </a>
-    </div>
+        </div>
+    </a>
 </div>

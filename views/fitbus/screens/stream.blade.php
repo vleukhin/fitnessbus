6b128539bd9232627b13fea801bd1cfb284a840c
vleukhin <norad-2013@mail.ru>
2017-08-18 14:28:41 +0300

    ссылка на трансляюцию

--- views/fitbus/screens/stream.blade.php
+++ views/fitbus/screens/stream.blade.php
@@ -7,8 +7,8 @@
         или не можешь попасть в ЖИРОБАС, следи за онлайн-трансляцией с места
         событий на канале «ЖИВИ!»t
     </div>
-    <a href="http://go2fit.pro" class="fitbus-btn">онлайн трансляция &nbsp;&nbsp;&nbsp;<i class="fa fa-long-arrow-right"></i></a>
+    <a href="https://www.jv.ru/vdnh" class="fitbus-btn">онлайн трансляция &nbsp;&nbsp;&nbsp;<i class="fa fa-long-arrow-right"></i></a>
     <div class="fitbus-screen-stream__date">
-        трансляция будет идти 23 и 24 августа 2017 года
+        трансляция будет идти 23, 24 и 25 августа 2017 года
     </div>
 </div>

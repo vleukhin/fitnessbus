8b873d5d290122e502e06431941af146fe4c7247
vleukhin <norad-2013@mail.ru>
2017-08-18 11:25:18 +0300

    второй экран

--- views/fitbus/screens/trainers.blade.php
+++ views/fitbus/screens/trainers.blade.php
@@ -2,12 +2,21 @@
 {{--Tel: +7-926-797-5419--}}
 {{--E-mail: vleukhin@ya.ru--}}
 <div class="fitbus-screen-trainers">
-    <div class="fitbus-title">Настя и Стас Сконечны</div>
-    <div class="fitbus-text">
-        профессиональные тренеры, лица проекта GO2FIT и счастливая семейная пара проведут персональные тренировки для самых смелых и активных горожан.
-    </div>
-    <div class="fitbus-screen-trainers__coaches">
-        @include('fitbus.blocks.coach', ['image' => '/images/nastya.png', 'name' => 'skonechnynastya'])
-        @include('fitbus.blocks.coach', ['image' => '/images/stas.png', 'name' => 'supersstas'])
+    <div class="fitbus-screen-trainers__content">
+        <div class="fitbus-screen-trainers__counters">
+            <span>3</span> дня <i>|</i> <span>2</span> тренера <i>|</i> <span>1</span> фит-стоп
+        </div>
+        <div class="fitbus-title">Настя и Стас Сконечны</div>
+        <div class="fitbus-text fitbus-text_dark fitbus-screen-trainers__text fitbus-screen-trainers__text_first">
+            профессиональные тренеры, лица проекта GO2FIT и счастливая семейная пара проведут персональные тренировки для самых смелых и активных горожан.
+        </div>
+        <div class="fitbus-screen-trainers__coaches">
+            @include('fitbus.blocks.coach', ['image' => '/images/nastya.png', 'name' => 'skonechnynastya'])
+            @include('fitbus.blocks.coach', ['image' => '/images/stas.png', 'name' => 'supersstas'])
+        </div>
+        <div class="fitbus-text fitbus-text_dark fitbus-screen-trainers__text fitbus-screen-trainers__text_second">
+            Первый в России фитнес-клуб на колесах оборудован профессиональными тренажерами официального партнера проекта:
+        </div>
+        <img src="/images/logos/lifefitness_dark.png" alt="" class="fitbus-screen-trainers__logo">
     </div>
 </div>

ccb142b424384026991131c6ab219cad531d56a8
vleukhin <norad-2013@mail.ru>
2017-08-18 15:38:46 +0300

    _blank

--- views/fitbus/screens/creators.blade.php
+++ views/fitbus/screens/creators.blade.php
@@ -8,7 +8,7 @@
         и узнай больше о проекте Go2fit
     </div>
     <div class="fitbus-screen-creators__buttons">
-        <a href="http://go2fit.pro" class="fitbus-btn">На сайт Go2Fit &nbsp;&nbsp;&nbsp;<i class="fa fa-long-arrow-right"></i></a>
-        <a href="mailto:info@go2fit.pro" class="fitbus-btn fitbus-btn_spacing">E-mail &nbsp;&nbsp;&nbsp;<i class="fa fa-envelope"></i></a>
+        <a href="http://go2fit.pro" class="fitbus-btn" target="_blank">На сайт Go2Fit &nbsp;&nbsp;&nbsp;<i class="fa fa-long-arrow-right"></i></a>
+        <a href="mailto:info@go2fit.pro" class="fitbus-btn fitbus-btn_spacing" target="_blank">E-mail &nbsp;&nbsp;&nbsp;<i class="fa fa-envelope"></i></a>
     </div>
 </div>

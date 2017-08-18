6100b4f66e6425965f63dae1ff273ff1fad50cbe
vleukhin <norad-2013@mail.ru>
2017-08-18 10:29:13 +0300

    шрифт GloberBlack для заголовков

--- views/fitbus/app.blade.php
+++ views/fitbus/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ЖИР0BUS</title>
     <script src="/build/app.bundle.js"></script>

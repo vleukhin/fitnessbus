0cd97f6fed88a20da3a520552e181df4c1a39713
vleukhin <norad-2013@mail.ru>
2017-08-18 15:02:23 +0300

    скрин с ссылками на почту и основной сайт

--- views/fitbus/landing.blade.php
+++ views/fitbus/landing.blade.php
@@ -9,8 +9,5 @@
     @include('fitbus.screens.steps')
     @include('fitbus.screens.location')
     @include('fitbus.screens.stream')
-
-    {{--<a href="http://go2fit.pro" class="fitbus-btn">На сайт Go2Fit &nbsp;&nbsp;&nbsp;<i class="fa fa-long-arrow-right"></i></a>--}}
-    {{--<a href="mailto:info@go2fit.pro" class="fitbus-btn fitbus-btn_spacing">E-mail &nbsp;&nbsp;&nbsp;<i class="fa fa-envelope"></i></a>--}}
-
+    @include('fitbus.screens.creators')
 @endsection
